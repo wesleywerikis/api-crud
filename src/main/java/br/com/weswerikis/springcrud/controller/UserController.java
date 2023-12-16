bcc146f1295a938a056c4114f497990ed08f748f
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-16 19:56:51 -0300

    Adjustment in the delete method, added PathVariable

--- src/main/java/br/com/weswerikis/springcrud/controller/UserController.java
+++ src/main/java/br/com/weswerikis/springcrud/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,8 +40,8 @@ public class UserController {
         return userService.update(userDTO);
     }
 
-    @DeleteMapping
-    public ResponseEntity<Void> delete(Long id){
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id){
         userService.delete(id);
         return ResponseEntity.ok().build();
     }
