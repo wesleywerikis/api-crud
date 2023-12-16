def0b81b3622219c68ad82e7de23d8674f2e2fb7
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-16 08:15:55 -0300

    Added param in variable related to database

--- src/main/java/br/com/weswerikis/springcrud/entity/User.java
+++ src/main/java/br/com/weswerikis/springcrud/entity/User.java
@@ -1,5 +1,6 @@
 package br.com.weswerikis.springcrud.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,9 +18,17 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String login;
+
+    @Column(nullable = false)
     private String passwoerd;
+    
+    @Column(nullable = false)
     private String email;
 
 }
