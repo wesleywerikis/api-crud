347f1e08f8336c646a86821da4e3d1ff02a181f6
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-16 08:41:45 -0300

    Created constructor empty

--- src/main/java/br/com/weswerikis/springcrud/entity/User.java
+++ src/main/java/br/com/weswerikis/springcrud/entity/User.java
@@ -36,6 +36,9 @@ public class User {
     @Column(nullable = false)
     private String email;
 
+    public User() {
+    }
+
     public User(UserDTO dto) {
         BeanUtils.copyProperties(dto, this);
     }
