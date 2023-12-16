457aa1f279c09ee9680e4d2b7c3716354ca8bc6e
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-16 19:09:51 -0300

    Created UserService and implements method listAll

---
+++ src/main/java/br/com/weswerikis/springcrud/service/UserService.java
@@ -0,0 +1,26 @@
+package br.com.weswerikis.springcrud.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import br.com.weswerikis.springcrud.dto.UserDTO;
+import br.com.weswerikis.springcrud.entity.User;
+import br.com.weswerikis.springcrud.repository.UserRepository;
+
+@Service
+public class UserService {
+    
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserDTO> listAll(){
+        List<User> users = userRepository.findAll();
+        return users.stream().map(UserDTO::new).toList();
+    }
+
+
+}
